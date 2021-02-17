8210a0e819aa38360339389866567ddc17fe5ca0
Artem Bakulin <artyom.bakulin@gmail.com>
2021-02-17 22:34:17 +0300

    More stats for Shiller CAPE

---
+++ index-investing/data/shiller_cape.R
@@ -0,0 +1,37 @@
+library(dplyr)
+library(readr)
+library(tidyr)
+
+SAMPLE_QUANTILES <- c(0.05, 0.25, 0.5, 0.75, 0.95)
+
+cape_yield_data <- read_csv("shiller_cape_excess_yield.csv")
+
+summarize_cape_yield <- function(cape_yield_data) {
+  cape_yield_data %>% 
+    summarise(
+      pct = sprintf("%d%%", 100*SAMPLE_QUANTILES),
+      cape_yield = sprintf("%.2f%%", 100*quantile(cape_excess_yield, SAMPLE_QUANTILES))
+    )
+}
+
+cape_yield_data %>% 
+  group_by(
+    period = case_when(
+      month >= '1986-01-01' & month <= '2020-12-01' ~ "1986--2020",
+      month >= '1951-01-01' & month <= '1985-12-01' ~ "1951--1985",
+      month >= '1916-01-01' & month <= '1950-12-01' ~ "1916--1950",
+      month >= "1881-01-01" & month <= '1915-12-01' ~ "1881--1915",
+      TRUE ~ "Other"
+    )
+  ) %>% 
+  summarize_cape_yield() %>% 
+  ungroup() %>% 
+  bind_rows(
+    cape_yield_data %>%
+      filter(month >= '1881-01-01', month <= '2020-12-01') %>% 
+      group_by(period = "1881--2020") %>% 
+      summarize_cape_yield()
+  ) %>% 
+  pivot_wider(names_from="pct", values_from="cape_yield") %>% 
+  filter(!period == "Other")
+
