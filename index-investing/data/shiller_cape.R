9118ae706ef9de729e436ec35d6ed76a7db34f5f
Artem Bakulin <artyom.bakulin@gmail.com>
2021-04-04 11:36:02 +0300

    Fix data quality issue in CAPE stratey

--- index-investing/data/shiller_cape.R
+++ index-investing/data/shiller_cape.R
@@ -58,8 +58,8 @@ CAPE_QUANTILES <- c(0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9)
 
 cape_yield_data %>% 
   filter(
-    year(month) >= 1926,
-    month(month) == 1,
+    year(month) >= 1927,
+    year(month) <= 2010,
     !is.na(cape_excess_yield),
     !is.na(subsequent_stock_return_10y)
   ) %>% 
@@ -84,13 +84,14 @@ rolling_ecdf <- function(x, width) {
 }
 
 market_returns <- read_csv("fama_french_four_factors_data.csv") %>% 
-  select(month, mkt_rf, rf)
-  
+  select(month, mkt_rf)
 
+# CAPE Excess Yield for Feb 2021 is based on average 10-year bond yield for this month
+# So we can trade on March 1 using CAPE excess yield as of February - hence the lag()
 backtest_data <- cape_yield_data %>% 
   select(month, cape_excess_yield) %>% 
   mutate(
-    cape_quantile = rolling_ecdf(cape_excess_yield, 40*12),
+    cape_quantile = lag(rolling_ecdf(cape_excess_yield, 40*12)),
     signal = case_when(
       cape_quantile >= 0.95 ~ 1.0,
       cape_quantile <= 0.05 ~ 0.0,
@@ -102,20 +103,20 @@ backtest_data <- cape_yield_data %>%
   ) %>% 
   inner_join(market_returns, by="month") %>% 
   mutate(
-    benchmark_return = 0.5*(mkt_rf+rf) + 0.5*rf,
-    strategy_return = signal*(mkt_rf+rf) + (1-signal)*rf
+    benchmark_return = 0.5*mkt_rf,
+    strategy_return = signal*mkt_rf
   )
 
 
 summarize_backtest_resuls <- function(data) {
   data %>% 
     summarise(
-      bench_mean = mean(benchmark_return - rf) * 12,
-      bench_std  = sd(benchmark_return - rf) * sqrt(12),
+      bench_mean = mean(benchmark_return) * 12,
+      bench_std  = sd(benchmark_return) * sqrt(12),
       bench_sharpe = bench_mean / bench_std,
       bench_drawdown = min(cumprod(1 + benchmark_return) / cummax(cumprod(1 + benchmark_return))) - 1,
-      strat_mean = mean(strategy_return - rf) * 12,
-      strat_std = sd(strategy_return - rf) * sqrt(12),
+      strat_mean = mean(strategy_return) * 12,
+      strat_std = sd(strategy_return) * sqrt(12),
       strat_sharpe = strat_mean / strat_std,
       strat_drawdown = min(cumprod(1 + strategy_return) / cummax(cumprod(1 + strategy_return))) - 1,
       mean_signal = mean(signal),
@@ -147,16 +148,16 @@ prepare_growth_data <- function(backtest_data) {
       date = last_day_of_month(month),
       benchmark_growth = cumprod(1 + benchmark_return),
       strategy_growth = cumprod(1 + strategy_return),
-      mkt_growth = cumprod(1 + mkt_rf + rf),
-      rf_growth = cumprod(1 + rf)
+      overperformance = strategy_growth / benchmark_growth,
+      signal = signal
     )
   
   tibble(
     date = min(backtest_data$month),
     benchmark_growth = 1,
     strategy_growth = 1,
-    mkt_growth = 1,
-    rf_growth = 1
+    overperformance = 1,
+    signal = NA
   ) %>% 
     bind_rows(growth_data)
 }
@@ -164,11 +165,11 @@ prepare_growth_data <- function(backtest_data) {
 backtest_data %>% 
   filter(month >= '1990-01-01') %>% 
   prepare_growth_data() %>% 
-  write_csv("cape_strategy_growth_1990.csv")
+  write_csv("cape_strategy_growth_1990.csv", na="")
 
 backtest_data %>% 
   filter(month >= '1927-01-01') %>% 
   prepare_growth_data() %>% 
-  write_csv("cape_strategy_growth_1927.csv")
+  write_csv("cape_strategy_growth_1927.csv", na="")
 
 
