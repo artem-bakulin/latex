cb3c2075f220f76fda2eae13d060fdb6ee6ef9dd
Artem Bakulin <artyom.bakulin@gmail.com>
2021-10-27 21:53:41 +0300

    Slides on theta

--- derivatives/fx_vol.R
+++ derivatives/fx_vol.R
@@ -116,13 +116,20 @@ tibble(strike = seq(55, 95, 0.1)) %>%
   write_csv("usdrub_implied_density.csv")
 
 
+black_scholes_d1 <- function(S, K, T, sigma, r, q) {
+ 
+  1 / (sigma*sqrt(T)) * (log(S/K) + (r - q + sigma*sigma/2)*T) 
+}
+
+black_scholes_d2 <- function(S, K, T, sigma, r, q) {
+  
+  1 / (sigma*sqrt(T)) * (log(S/K) + (r - q - sigma*sigma/2)*T) 
+}
 
 black_scholes_delta <- function(S, K, T, sigma, r, q) {
   
-  d1 <- 1 / (sigma*sqrt(T)) * (log(S/K) + (r - q + sigma*sigma/2)*T)
-  pnorm(d1)
+  pnorm(black_scholes_d1(S, K, T, sigma, r, q))
 }
-
 
 black_scholes_vega <- function(S, K, T, sigma, r, q) {
   
@@ -130,6 +137,13 @@ black_scholes_vega <- function(S, K, T, sigma, r, q) {
   S * dnorm(d1) * sqrt(T)
 }
 
+black_scholes_theta <- function(S, K, T, sigma, r, q) {
+
+  d1 <- black_scholes_d1(S, K, T, sigma, r, q)
+  d2 <- black_scholes_d2(S, K, T, sigma, r, q)
+  
+  -S * dnorm(d1) * sigma / (2*sqrt(T)) - r*K*exp(-r*T)*pnorm(d2)
+}
 
 tibble(S = seq(0, 1, 0.01)) %>% 
   mutate(
@@ -143,3 +157,21 @@ tibble(S = seq(0, 1, 0.01)) %>%
     vega = black_scholes_vega(S, 0.5, 1, 0.2, 0, 0)
   ) %>% 
   write_csv("black_scholes_vega.csv")
+
+tibble(S = seq(0, 1, 0.01)) %>% 
+  mutate(
+    theta = black_scholes_theta(S, K=0.5, T=1, sigma=0.2, r=0, q=0)
+  ) %>% 
+  write_csv("black_scholes_theta.csv")
+
+tibble(T = seq(0.01, 1, 0.01)) %>% 
+  mutate(
+    theta_itm = black_scholes_theta(S=0.55, K=0.5, T, sigma=0.3, r=0, q=0),
+    theta_atm = black_scholes_theta(S=0.50, K=0.5, T, sigma=0.3, r=0, q=0),
+    theta_otm = black_scholes_theta(S=0.45, K=0.5, T, sigma=0.3, r=0, q=0)
+  ) %>% 
+  write_csv("black_scholes_theta_by_money.csv")
+#  ggplot(aes(x=T)) +
+#  geom_line(aes(y=theta_itm), color="red") +
+#  geom_line(aes(y=theta_atm), color="blue") +
+#  geom_line(aes(y=theta_otm))
