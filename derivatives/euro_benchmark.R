b512b2f0c880c47e107770248843f1a45de90e3c
Artem Bakulin <artyom.bakulin@gmail.com>
2022-03-16 22:47:10 +0300

    Update EURIBOR charts using Bundesbank data

--- derivatives/euro_benchmark.R
+++ derivatives/euro_benchmark.R
@@ -1,14 +1,50 @@
 library(dplyr)
 library(readr)
+library(curl)
 
-eonia <- read_csv("eonia.csv") %>% arrange(date)
 
-ester <- read_csv("ester.csv") %>% arrange(date)
+download_from_bundesbank <- function(series_url, series_name) {
+  
+  curl_download(series_url, "tmp.csv")
+  
+  data <- read_csv("tmp.csv", skip=8, col_names=c("date", "value", "comment"))
+  
+  data <- data %>% 
+    mutate(value = as.numeric(value)) %>% 
+    filter(!is.na(value)) %>% 
+    select(date, value)
+  
+  colnames(data) <- c("date", series_name)
+  
+  file.remove("tmp.csv")
+  
+  data
+}
 
-euribor <- read_csv("euribor.csv") %>% arrange(date)
+euribor_3m <- download_from_bundesbank(
+  "https://api.statistiken.bundesbank.de/rest/download/BBK01/ST0316?format=csv&lang=en",
+  "3m"
+)
+
+euribor_6m <- download_from_bundesbank(
+  "https://api.statistiken.bundesbank.de/rest/download/BBK01/ST0325?format=csv&lang=en",
+  "6m"
+)
+
+eonia <- download_from_bundesbank(
+  "https://api.statistiken.bundesbank.de/rest/download/BBK01/ST0304?format=csv&lang=en",
+  "eonia"
+)
+
+ester <- download_from_bundesbank(
+  "https://api.statistiken.bundesbank.de/rest/download/BBMMB/D.EU000A2X2A25.WT?format=csv&lang=en",
+  "ester"
+)
 
 eonia %>% 
-  left_join(ester, by="date") %>% 
-  inner_join(euribor, by="date") %>%
+  full_join(ester, by="date") %>% 
+  inner_join(euribor_3m, by="date") %>% 
+  inner_join(euribor_6m, by="date") %>% 
   mutate(ester = if_else(is.na(ester), eonia - 0.085, ester)) %>% 
-  write_csv("euro_benchmark.csv")
+  filter(!is.na(ester)) %>% 
+  write_csv("euro_benchmark.csv", na="")
