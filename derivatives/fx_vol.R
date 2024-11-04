8ba161cd9ef9ee47bc47416f5326fe644d0dd06a
Artem Bakulin <artyom.bakulin@gmail.com>
2024-11-04 21:14:25 +0100

    Update more slides

--- derivatives/fx_vol.R
+++ derivatives/fx_vol.R
@@ -71,6 +71,7 @@ get_central_bank_fx_rate("USD", 2012, 2024) %>%
   group_by(mid_month = make_date(year(date), month(date), 15)) %>% 
   summarise(realized_vol = sd(log_return) * sqrt(250)) %>% 
   filter(!is.na(realized_vol)) %>% 
+  filter(mid_month != max(mid_month)) %>% 
   write_csv("USDRUB_realized_vol.csv")
 
 
