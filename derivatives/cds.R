42bdd013be566b05fc717ddbb316fb9aa59acf7a
Artem Bakulin <artyom.bakulin@gmail.com>
2026-04-03 15:35:47 +0200

    Update corp yields data and slides on CDS

--- derivatives/cds.R
+++ derivatives/cds.R
@@ -1,6 +1,7 @@
 library(dplyr)
 library(readr)
 library(lubridate)
+library(curl)
 
 last_day_of_month <- function(x) {
   
@@ -11,16 +12,33 @@ last_day_of_month <- function(x) {
   )
 }
 
-high_yield_data <- read_csv("bofa_high_yield_index.csv") %>% 
+download_fred_data <- function(series_name, frequency="Monthly", mode="eop") {
+  
+  fred_url <- "https://fred.stlouisfed.org/graph/fredgraph.csv?&id={series_name}&fq={frequency}&fam={mode}"
+  fred_url <- gsub("{series_name}", series_name, fred_url, fixed=TRUE)
+  fred_url <- gsub("{frequency}", frequency, fred_url, fixed=TRUE)
+  fred_url <- gsub("{mode}", mode, fred_url, fixed=TRUE)
+  
+  tmp_file <- tempfile("series_name", fileext=".csv")
+  curl_download(fred_url, tmp_file)
+  
+  data <- read_csv(tmp_file)
+  file.remove(tmp_file)
+  data
+}
+
+high_yield_data <- 
+  download_fred_data("BAMLHYH0A0HYM2TRIV") %>%
   transmute(
-    month = last_day_of_month(DATE),
+    month = last_day_of_month(observation_date),
     high_yield_index = as.numeric(BAMLHYH0A0HYM2TRIV)
   ) %>% 
   filter(!is.na(high_yield_index))
 
-corp_data <-read_csv("bofa_corporate_index.csv") %>% 
+corp_data <- 
+  download_fred_data("BAMLCC0A0CMTRIV") %>%
   transmute(
-    month = last_day_of_month(DATE),
+    month = last_day_of_month(observation_date),
     corp_index = as.numeric(BAMLCC0A0CMTRIV)
   ) %>% 
   filter(!is.na(corp_index))
